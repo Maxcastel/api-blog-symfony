645e0ef15635a986031589943285b80090a9493a
Maxcastel <maxence.nord@gmail.com>
2024-08-22 15:42:41 +0200

    ArticleController: add getAllArticles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,18 +11,21 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 use App\Repository\UserRepository;
+use App\Repository\ArticleRepository;
 
 class ArticleController extends AbstractController
 {
     private $em;
     private $serializer;
     private $userRepository;
+    private $articleRepository;
 
-    public function __construct(EntityManagerInterface $em, SerializerInterface $serializer, UserRepository $userRepository)
+    public function __construct(EntityManagerInterface $em, SerializerInterface $serializer, UserRepository $userRepository, ArticleRepository $articleRepository)
     {
         $this->em = $em;
         $this->serializer = $serializer;
         $this->userRepository = $userRepository;
+        $this->articleRepository = $articleRepository;
     }
 
     #[Route('/api/articles', methods:["POST"], name: 'article_create')]
@@ -47,6 +50,26 @@ class ArticleController extends AbstractController
                 'message' => 'Created with success',
                 "data" => $article,
             ], Response::HTTP_CREATED, [], ['groups' => 'getArticle']);
+        }   
+        catch(\Exception $e){
+            return $this->json([
+                "status" => 400,
+                "success" => false,
+                'message' => $e->getMessage(),
+            ], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    #[Route('/api/articles', methods:["GET"], name: 'article_show_all')]
+    public function getAllArticles(): JsonResponse
+    {
+        try{
+            return $this->json([
+                "status" => 200,
+                "success" => true,
+                'message' => 'Operation completed with success',
+                "data" => $this->articleRepository->findAll(),
+            ], Response::HTTP_OK, [], ['groups' => 'getArticle']);
         }   
         catch(\Exception $e){
             return $this->json([
