6804e19fcf869316db8ba193701bd7ac0f338123
Maxcastel <maxence.nord@gmail.com>
2024-08-22 23:23:54 +0200

    EmailController: add sendEmail

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -2,18 +2,67 @@
 
 namespace App\Controller;
 
+use App\Repository\EmailRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Entity\Email;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email as MimeEmail;
 
 class EmailController extends AbstractController
 {
-    #[Route('/email', name: 'app_email')]
-    public function index(): JsonResponse
+    private $em;
+    private $serializer;
+    private $emailRepository;
+
+    public function __construct(EmailRepository $emailRepository, EntityManagerInterface $em, SerializerInterface $serializer)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/EmailController.php',
-        ]);
+        $this->em = $em;
+        $this->serializer = $serializer;
+        $this->emailRepository = $emailRepository;
+    }
+
+    #[Route('/api/emails', methods:["POST"], name: 'email_send')]
+    public function sendEmail(Request $request, MailerInterface $mailer): JsonResponse
+    {
+        try{
+            $email = $this->serializer->deserialize($request->getContent(), Email::class, 'json');
+
+            date_default_timezone_set('Europe/Paris');
+
+            $email->setsendDate(new \DateTime());
+
+            $this->em->persist($email);
+            $this->em->flush();
+
+            $data = json_decode($request->getContent(), true);
+
+            $emailToSend = (new MimeEmail())
+                ->from($data["email"])
+                ->to('maxence.castel59@gmail.com')
+                ->subject($data["subject"])
+                ->html($data["message"]);
+
+            $mailer->send($emailToSend);
+
+            return $this->json([
+                "status" => 201,
+                "success" => true,
+                "data" => $email,
+                'message' => 'Email sent with success',
+            ], Response::HTTP_CREATED);
+        }
+        catch(\Exception $e){
+            return $this->json([
+                "status" => 400,
+                "success" => false,
+                'message' => $e->getMessage(),
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
